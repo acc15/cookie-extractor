626ec6ad3f1e820d54ed4a8299b1dec72c2fd6ee
Vyacheslav Mayorov <vyacheslav.mayorov@gmail.com>
2022-09-29 18:08:52 +0300

    cookie filter

--- cookie-extractor-chrome/src/background.ts
+++ cookie-extractor-chrome/src/background.ts
@@ -1,10 +1,16 @@
 import { Config, loadConfig } from "./config";
 import CookieManager from "./cookie-manager";
 import { CookieRequest } from "./model";
+import { isCookieMatches, isSameCookie } from "./util";
 
 async function fetchCookies(cfg: Config): Promise<CookieRequest> {
-    const arrayOfCookies = await Promise.all(cfg.cookies.map(c => new Promise<Array<chrome.cookies.Cookie>>((resolve) => chrome.cookies.getAll(c, resolve))));
-    return { cookies: arrayOfCookies.flatMap(c => c), clientId: cfg.clientId };
+    const cookieLists = await Promise.all(cfg.cookies.map(c => new Promise<Array<chrome.cookies.Cookie>>((resolve) => chrome.cookies.getAll(c, resolve))));
+    for (let i = cookieLists.length - 1; i > 0; i--) {
+        for (let j = i - 1; j >= 0; j--) {
+            cookieLists[j] = cookieLists[j].filter(c1 => !cookieLists[i].some(c2 => isSameCookie(c1, c2)))
+        }
+    }
+    return { cookies: cookieLists.flatMap(c => c), clientId: cfg.clientId };
 }
 
 async function init() {
@@ -17,8 +23,11 @@ async function init() {
     const manager = new CookieManager(() => fetchCookies(cfg), cfg);
     await manager.sendNow();
 
-    chrome.cookies.onChanged.addListener(async (ci) => {
-        console.log("Cookie changed");
+    chrome.cookies.onChanged.addListener(ci => {
+        if (!cfg.cookies.some(c => isCookieMatches(ci.cookie, c))) {
+            return;
+        }
+        console.debug("Cookie changed", ci);
         manager.debounceSend();
     });
 }
--- cookie-extractor-chrome/src/util.ts
+++ cookie-extractor-chrome/src/util.ts
@@ -1,8 +1,18 @@
 
-export function shallowEq(obj1: {[k: string]: any}, obj2: {[k: string]: any}): boolean {
-    const keys = new Set(Object.keys(obj1).concat(Object.keys(obj2)));
+export function isCookieMatches(cookie: chrome.cookies.Cookie, details: chrome.cookies.GetAllDetails): boolean {
+    return (details.domain === undefined || (cookie.hostOnly ? details.domain === cookie.domain : cookie.domain.endsWith(details.domain)))
+        && (details.name === undefined || details.name === cookie.name)
+        && (details.path === undefined || details.path === cookie.path)
+        && (details.storeId === undefined || details.storeId === cookie.storeId)
+        && (details.secure === undefined || details.secure === cookie.secure)
+        && (details.session === undefined || details.session === cookie.session)
+        && (details.url === undefined || (cookie.hostOnly ? details.url === (cookie.domain + cookie.path) : details.url.endsWith(cookie.domain + cookie.path)));
+}
+
+export function isSameCookie(c1: chrome.cookies.Cookie, c2: chrome.cookies.Cookie): boolean {
+    const keys: Array<keyof chrome.cookies.Cookie> = ["domain", "name", "path"];
     for (const k of keys) {
-        if (obj1[k] !== obj2[k]) {
+        if (c1[k] !== c2[k]) {
             return false;
         }
     }
