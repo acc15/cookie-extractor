dd004a36a295611d285852783de7b6c1754961c4
Vyacheslav Mayorov <vyacheslav.mayorov@gmail.com>
2022-09-29 18:09:26 +0300

    deleted rsc script as useless

--- scripts/rsc.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-URL="ws://localhost:8080/rsocket"
-DATA='[{"domain":"domain","name":"name","path":"path","value":"value","hostOnly":true,"httpOnly":true,"secure":true,"session":true,"storeId":"storeId","sameSite":"unspecified"}]'
-
-rsc -w -r="cookie" --dmt='application/json' -d="${DATA}" "${URL}" 
